496e3a76d858b3e8d193d122733a893d49e6dd09
Rafał Rybicki <rafarybicki@gmail.com>
2020-01-14 17:37:38 +0100

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import HeaderNav from './containers/HeaderNav/HeaderNav';
+import {SideBar} from './containers/SideBar/SideBar';
 
 function App() {
   return (
-    <HeaderNav/>
-  );
+    <React.Fragment>
+     <HeaderNav/>
+     <SideBar/>
+   </React.Fragment>
+ );
 }
 
 export default App;
