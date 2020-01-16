a1e8c56a763d63dcf0effe83c28d73f9cdd5df58
Rafał Rybicki <rafarybicki@gmail.com>
2020-01-16 13:34:47 +0100

    add youtube library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import {Home} from './containers/Home/Home';
 import {AppLayout} from './components/AppLayout/AppLayout';
-import {Watch} from './containers/Watch/Watch'
 import {Route, Switch} from 'react-router-dom';
+import {Watch} from './containers/Watch/Watch';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {youtubeLibraryLoaded} from './store/actions/api';
 
 class App extends Component {
   render() {
@@ -15,6 +18,29 @@ class App extends Component {
       </AppLayout>
     );
   }
+  componentDidMount() {
+    this.loadYoutubeApi();
+  }
+
+  loadYoutubeApi() {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/client.js";
+
+    script.onload = () => {
+      window.gapi.load('client', () => {
+        window.gapi.client.setApiKey(process.env.REACT_APP_API_KEY);
+        window.gapi.client.load('youtube', 'v3', () => {
+          this.props.youtubeLibraryLoaded();
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({youtubeLibraryLoaded}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
