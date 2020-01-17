72e1fe2509470609e43f75a21aea76b3515ee46d
Rafał Rybicki <rafarybicki@gmail.com>
2020-01-17 09:13:08 +0100

    prepare toutube api

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,17 @@
-import {all} from 'redux-saga/effects';
-export default function* (){
-  yield all([]);
+import {all, call, put, fork} from 'redux-saga/effects';
+import {watchMostPopularVideos} from './video';
+export default function* () {
+  yield all([
+    fork(watchMostPopularVideos),
+  ]);
+}
+
+export function* fetchEntity(request, entity, ...args) {
+  try {
+    const response = yield call(request);
+
+    yield put(entity.success(response.result, ...args));
+  } catch (error) {
+    yield put(entity.failure(error, ...args));
+  }
 }
